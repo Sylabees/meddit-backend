550e65cce408f0dde0a90ad7e1abdc86ab226ccd
Matt M <matthewdsmorgan@gmail.com>
2025-05-30 23:58:49 +0100

    added POST to add a element

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask import request
 
 app = Flask('meddit')
 CORS(app)  # allows frontend to connect
@@ -37,4 +38,21 @@ def delete_one_post(single_id):
         return jsonify({"error": f'ID {single_id} could not be found!'}), 404    
     posts = updated_posts
     return jsonify({"message": f'id {single_id} has been deleted'}), 200
+
+# Add one post
+@app.route('/posts', methods = ['POST'])
+def create_post():
+    global posts
+    body = request.get_json()
+
+    if isinstance(body, dict):
+        next_id = max([x["id"] for x in posts], default = 0) + 1
+        body["id"] = next_id
+        posts.append(body)
+        return jsonify({"message": f"ID {next_id} added"}), 201
+    else:
+        return jsonify({"error": "unable to add"}), 400
+    
+
+
 
